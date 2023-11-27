2da04b885210400994dd240b308226a8ad399dcc
consorcioCAP1 <omar.dcruz12@gmail.com>
2023-11-27 10:41:42 -0500

    primer commit de prueba agregado 1.0

--- src/main/java/com/nttdata/bootcamp/clientservice/ClientServiceApplication.java
+++ src/main/java/com/nttdata/bootcamp/clientservice/ClientServiceApplication.java
@@ -10,6 +10,8 @@ public class ClientServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientServiceApplication.class, args);
+
+		//comentario agregado para probar el primer commit 
 	}
 
 }
